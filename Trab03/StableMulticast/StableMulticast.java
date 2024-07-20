800c866acddc3723eec6672adf9b92b1d98c436d
Joao Davi Rigo Mazzarolo <jdrmazzarolo@outlook.com>
2024-07-20 20:57:56 -0300

    adding package

---
+++ Trab03/StableMulticast/StableMulticast.java
@@ -0,0 +1,18 @@
+package StableMulticast;
+
+public class StableMulticast implements IStableMulticast{
+    
+    public StableMulticast(String ip, Integer port, IStableMulticast client){
+
+    }
+
+
+    public void msend(String msg, IStableMulticast cliente){
+        
+    }
+
+
+    public void deliver(String msg){
+
+    }
+}
